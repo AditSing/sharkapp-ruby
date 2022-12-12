a2cf28ba4365f973ea0b7b61299f7d11af7dd5a1
aditsing <adityasinggiprayogi@gmail.com>
2022-12-12 05:37:59 -0800

    Create Nested Resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :sharks
+  resources :sharks do
+    resources :posts
+  end
   root 'sharks#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
